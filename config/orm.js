283356cae7227d21972cd0feae9a9b43fd79cb3d
Adrian Quezada <adrianquezada38@gmail.com>
2019-06-01 18:33:41 -0400

    thrid commit

---
+++ config/orm.js
@@ -0,0 +1,75 @@
+var connection = require("../config/connection.js");
+
+
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+
+var orm = {
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(tableInput, value, cb) {
+    var queryString = "INSERT INTO " + tableInput;
+
+    queryString += ' (burger_name, devoured) VALUES ("';
+    queryString += value;
+    queryString += '", false); ';
+
+    console.log(queryString);
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  updateOne: function(tableInput, col, condition, cb) {
+    var queryString = "UPDATE " + tableInput;
+
+    queryString += " SET ";
+    queryString += objToSql(col);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
+
+
+module.exports = orm;
